6fc9d82708a6cd71042b288a4a0e9c4efbb35c8e
Olesya Miller <olesyam.miller@gmail.com>
2019-12-21 00:23:34 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require 'pry'
+
+class CashRegister
+  
+attr_writer :discount, :total
+attr_reader :discount, :total, :items, :last 
+
+#why do we need a :total writer method?
+
+  def initialize(discount = nil)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1) 
+   
+    @last = price*quantity 
+    @total += @last 
+    
+    quantity.times do 
+      @items << title
+    end
+  end
+  
+  def apply_discount
+    if @discount != nil 
+      @total -= @total/100 * @discount
+      return "After the discount, the total comes to $#{@total}."
+    else 
+      return "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @last 
+  end
+
+end
+
+
+
+
+
 
